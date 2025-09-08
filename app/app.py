cd358a4015b44c28c9288f9494192e540e5853c6
Jeevanfrankline1988 <franklineit07@gmail.com>
2025-09-08 19:09:16 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,12 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return "Game Running", 200
+
+if __name__ == "__main__":
+    # Flask keeps the container alive
+    app.run(host="0.0.0.0", port=5000)
