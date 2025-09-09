097d843dbd725d82fd961e8032b4241a52515d6a
Jeevanfrankline1988 <franklineit07@gmail.com>
2025-09-10 00:06:14 +0530

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
+import sgame
 
 app = Flask(__name__)
 
 @app.route("/")
-def health():
-    return "Game Running", 200
+def home():
+    return "Snake Game running! Visit /frame to see a snapshot."
+
+@app.route("/frame")
+def frame():
+    sgame.update_game()
+    img_bytes = sgame.render_frame()
+    return Response(img_bytes, mimetype="image/jpeg")
 
 if __name__ == "__main__":
-    # Flask keeps the container alive
     app.run(host="0.0.0.0", port=5000)
+
